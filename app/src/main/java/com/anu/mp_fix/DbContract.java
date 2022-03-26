0731991261ead8579afbc0735b155ae6137f4771
arpo1620@gmail.com <arpo1620@gmail.com>
2022-03-26 14:10:30 +0700

    login dan regist

---
+++ app/src/main/java/com/anu/mp_fix/DbContract.java
@@ -0,0 +1,6 @@
+package com.anu.mp_fix;
+
+public class DbContract {
+    public static final String SERVER_LOGIN_URL = "http://192.168.0.111/cape/checkLogin.php";
+    public static final String SERVER_REGISTER_URL = "http://192.168.0.111/cape/createData.php";
+}
--- app/src/main/java/com/anu/mp_fix/MainActivity.java
+++ app/src/main/java/com/anu/mp_fix/MainActivity.java
@@ -1,42 +1,41 @@
 package com.anu.mp_fix;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.widget.Button;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button inc, dec;
-    TextView tv;
-    int count;
-
-    @Override
+    Button btn1, btn2;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inc = findViewById(R.id.increment);
-        dec = findViewById(R.id.decrement);
-        tv = findViewById(R.id.textView);
+        btn1=(Button)findViewById(R.id.btn_1);
+        btn2=(Button)findViewById(R.id.btn_2);
 
-        inc.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                count++;
-                tv.setText(count + " Clicks!");
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
         });
 
-        inc.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                count--;
-                tv.setText(count + " Clicks!");
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(MainActivity.this,loginActivity.class);
+                startActivity(signUp);
             }
         });
-
-
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,regisActivity.class);
+                startActivity(signIn);
+            }
+        });
     }
 }
