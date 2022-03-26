a08f1d2d80ed84334355fa99be4c550412f88bbf
arpo1620@gmail.com <arpo1620@gmail.com>
2022-03-26 13:15:07 +0700

    tes button + dan -

--- app/src/main/java/com/anu/mp_fix/MainActivity.java
+++ app/src/main/java/com/anu/mp_fix/MainActivity.java
@@ -3,12 +3,40 @@ package com.anu.mp_fix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button inc, dec;
+    TextView tv;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inc = findViewById(R.id.increment);
+        dec = findViewById(R.id.decrement);
+        tv = findViewById(R.id.textView);
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                count++;
+                tv.setText(count + " Clicks!");
+
+            }
+        });
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                count--;
+                tv.setText(count + " Clicks!");
+            }
+        });
+
+
     }
 }
